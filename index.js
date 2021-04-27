e57ba23200efc3f887bf915331d65d2a83b0a3f6
Christian Bermeo <czaudiovisual@hotmail.com>
2021-04-27 01:51:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+
+// function superbowlWin(record){
+//     return(record === "W");
+// }
+// superbowlWin(record);
+
+
+// function superbowlWin(recordArray){
+//     const result = recordArray.find(isWin)
+//     if (result) {
+//         return result.year
+//     } else {
+//         return result
+//     }
+// }
+
+
+// function isWin(gameObject){
+//      return gameObject.result === "w"
+// }
+
+function superbowlWin(recordArray) {
+    const result = recordArray.find(gameObject => gameObject.result === "W")
+    return (result ? result.year : result)
+}
+
